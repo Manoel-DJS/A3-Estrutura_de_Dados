d66ef22af75251732b6247f8da8d3f5bb70a610c
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-30 13:52:54 -0300

    feat: logica para alocar caminhao ok

--- src/main/java/A3/me/AlocadorCaminhao.java
+++ src/main/java/A3/me/AlocadorCaminhao.java
@@ -7,6 +7,21 @@ public class AlocadorCaminhao {
     public static List<Caminhao> alocar(List<Armazem> armazens, int capacidadeCaminhao) {
         List<Caminhao> caminhoes = new ArrayList<>();
 
+        for (Armazem a : armazens) {
+            boolean alocado = false;
+            for (Caminhao c : caminhoes) {
+                if (c.adicionar(a, capacidadeCaminhao)) {
+                    alocado = true;
+                    break;
+                }
+            }
+            if (!alocado) {
+                Caminhao novo = new Caminhao();
+                novo.adicionar(a, capacidadeCaminhao);
+                caminhoes.add(novo);
+            }
+        }
+
         return caminhoes;
     }
 }
--- src/main/java/A3/me/Caminhao.java
+++ src/main/java/A3/me/Caminhao.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 public class Caminhao {
     private List<Armazem> armazens = new ArrayList<>();
+
     private int cargaAtual = 0;
+    public boolean adicionar(Armazem a, int capacidadeMaxima) {
+        if (cargaAtual + a.getCapacidadeAtual() <= capacidadeMaxima) {
+            armazens.add(a);
+            cargaAtual += a.getCapacidadeAtual();
+            return true;
+        }
+        return false;
+    }
 
     public int getCargaAtual() {
         return cargaAtual;
