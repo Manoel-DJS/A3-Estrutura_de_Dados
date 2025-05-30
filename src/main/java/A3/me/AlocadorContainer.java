d9631385b7108f9819df0b007bd1e296f0190c11
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-30 11:56:24 -0300

    feat: logica para alocar container

--- src/main/java/A3/me/AlocadorContainer.java
+++ src/main/java/A3/me/AlocadorContainer.java
@@ -10,6 +10,22 @@ public class AlocadorContainer {
         Collections.sort(ordenados, Collections.reverseOrder()); // Ordena do maior para o menor
 
         List<Armazem> armazens = new ArrayList<>();
+
+        for (int volume : ordenados) {
+            boolean alocado = false;
+            for (Armazem a : armazens) {
+                if (a.adicionar(volume, capacidadeArmazem)) {
+                    alocado = true;
+                    break;
+                }
+            }
+            if (!alocado) {
+                Armazem novo = new Armazem();
+                novo.adicionar(volume, capacidadeArmazem);
+                armazens.add(novo);
+            }
+        }
+
         return armazens;
     }
 }
