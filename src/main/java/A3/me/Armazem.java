e3dba56541e95f24eb400834d8006d2f7af5fcd1
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-30 10:22:17 -0300

    Create class Armazem

---
+++ src/main/java/A3/me/Armazem.java
@@ -0,0 +1,11 @@
+package A3.me;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Armazem {
+    private List<Integer> containers = new ArrayList<>();
+    private int capacidadeAtual = 0;
+
+
+}
