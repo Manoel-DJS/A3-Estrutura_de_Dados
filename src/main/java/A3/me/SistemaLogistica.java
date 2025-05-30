f28ae47c3ee4c98158c1feaf1dc9aca28a77554c
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-30 14:00:41 -0300

    feat: criando metodo estatico para exibir os resultados

--- src/main/java/A3/me/SistemaLogistica.java
+++ src/main/java/A3/me/SistemaLogistica.java
@@ -18,8 +18,30 @@ public class SistemaLogistica {
         System.out.println(capacidadeArmazem);
         System.out.println(capacidadeCaminhao);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        List<Armazem> armazens = AlocadorContainer.alocar(volumesContainers, capacidadeArmazem);
+        List<Caminhao> caminhoes = AlocadorCaminhao.alocar(armazens, capacidadeCaminhao);
+
+        exibirResultados(armazens, caminhoes);
+    }
+
+    public static void exibirResultados(List<Armazem> armazens, List<Caminhao> caminhoes) {
+        System.out.println("\n--- DISTRIBUIÇÃO DE CONTAINERS NOS ARMAZÉNS ---");
+        for (int i = 0; i < armazens.size(); i++) {
+            Armazem a = armazens.get(i);
+            System.out.println("Armazém " + (i + 1) + ": " + a.getContainers() + " = " + a.getCapacidadeAtual());
         }
+        System.out.println("Total de armazéns utilizados: " + armazens.size());
+
+        System.out.println("\n--- DISTRIBUIÇÃO DOS ARMAZÉNS NOS CAMINHÕES ---");
+        for (int i = 0; i < caminhoes.size(); i++) {
+            Caminhao c = caminhoes.get(i);
+            StringBuilder desc = new StringBuilder("Caminhão " + (i + 1) + " transporta: ");
+            for (Armazem a : c.getArmazens()) {
+                desc.append(a.getCapacidadeAtual()).append(" ");
+            }
+            System.out.println(desc.toString().trim() + " = " + c.getCargaAtual());
+
+        }
+        System.out.println("Total de caminhões utilizados: " + caminhoes.size());
     }
 }
