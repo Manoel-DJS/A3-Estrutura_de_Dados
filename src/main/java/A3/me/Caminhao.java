f508c3aa882fb51c253dcd5f6d735431f076abd2
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-30 10:28:20 -0300

    create class Caminhao

---
+++ src/main/java/A3/me/Caminhao.java
@@ -0,0 +1,8 @@
+package A3.me;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Caminhao {
+    private List<Armazem> armazens = new ArrayList<>();
+    private int cargaAtual = 0;
+}
