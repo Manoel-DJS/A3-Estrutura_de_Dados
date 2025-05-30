6518963a870c9b2f24434902a6c03783f1b95174
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-30 10:19:13 -0300

    Refatorando classe principal

---
+++ src/main/java/A3/me/SistemaLogistica.java
@@ -0,0 +1,12 @@
+package A3.me;
+
+public class SistemaLogistica {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("Bem vindo ao sistema de Otimização de logística portuária!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
